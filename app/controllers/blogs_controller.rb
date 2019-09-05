d587c1c4662896e78956ec10ff918bee5c6a52f7
Takeru Yamanaka <Takeru@takeru-no-MacBook-Pro-Retina.local>
2019-09-05 18:13:01 +0900

    [finish]4-3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,8 +6,28 @@ class BlogsController < ApplicationController
   end
 
   def new
+    @blog = Blog.new
+    # newにはparamsを指定しなくてOK
+  end
+
+  def create
+    blog = Blog.new(blog_params)
+    # 変数はローカルでOK、viewには渡さないため
+    blog.save
+    #大文字にしなくてOK
+    redirect_to blogs_path
+    # blogs_pathがどこから登場したのか不明
+
   end
 
   def edit
   end
+end
+
+private
+
+def blog_params
+  params.require(:blog).permit(:title, :category, :body)
+  # 区切りには"."を使う。requireするのはモデル名（コントローラ名：blogs）
+  # 許可するカラム名の区切りには”,”を使う
 end
