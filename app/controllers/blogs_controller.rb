c8c3f7dc1ad074afcb3cd925ae3b8cefb27ba7d2
Takeru Yamanaka <Takeru@takeru-no-MacBook-Pro-Retina.local>
2019-09-05 21:24:02 +0900

    [finish]4−6

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -23,9 +23,28 @@ class BlogsController < ApplicationController
   end
 
   def edit
+    @blog = Blog.find(params[:id])
+    # blog.update
+    # redirect_to blog_path
+
+    # updateはedit内の一つではなく、アクションの一つだと覚えておく。
+    # 書く場所は間違えたけど、記述はほぼほぼ合ってる。
+    # Link＿to”編集”がクリックされた際に実行されるアクションがedit.
+    # モデルに指示を出してdbからデータを引っ張ってきて”表示”する（※@が必要な理由）
+
+  end
+
+  def update
     blog = Blog.find(params[:id])
-    blog.update
-    redirect_to blog_path
+    # もしかしてリダイレクト処理が入ると、新しいviewファイルを呼び出さないので、インスタンス変数ではなくローカル変数を使う・・・？
+    blog.update(blog_params)
+    # blog_paramsで更新内容を制限する。createの時と同じ。
+    # edit.html.erbで保存（更新）をクリックされたときに呼び出されるアクション
+    # モデルに指示を出してdbからデータを引っ張り出す
+    # .update(blog_params)メソッドで上書きして格納
+    redirect_to blog_path(blog)
+    # 処理後はshow.html.erbへとリダイレクト
+    # blogs_pathはshow.htme.erbの名前付きルート
   end
 
   # def destroy
